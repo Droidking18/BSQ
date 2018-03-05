8ae2d07e71bb2e5bfb7977d5eeb9eccf5e1d53de
Dovi Kaplan <dovikaplan@gmail.com>
2018-03-05 15:59:26 +0200

    add more

---
+++ bsq/README.md
@@ -0,0 +1,2 @@
+# BSQ
+Resources for BSQ
---
+++ bsq/utility.h
@@ -0,0 +1,9 @@
+#ifndef UTILITY_H
+# define UTILITY_H
+
+int	ft_atoi(char *str);
+int	ft_find_map_start(char *str);
+int	ft_isdigit(char c);
+void	ft_find_valid_char(char *str, char *empty, char *obs, char *square);
+
+#endif
