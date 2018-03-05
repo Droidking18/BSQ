70e3df87aaf638db68807e8eb0887bf3321a598f
TlStephen2011 <tlstephen11@gmail.com>
2018-03-05 21:18:09 +0200

    Built grid and validated input

---
+++ BSQ_Tyrone/converter.h
@@ -0,0 +1,7 @@
+#ifndef CONVERTER_H
+# define CONVERTER_H
+
+void	ft_fill_int_grid(int **grid, char *str);
+void	ft_pad_grid(int **grid, char *str);
+
+#endif
---
+++ BSQ_Tyrone/main.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "utility.h"
+#include "validator.h"
+#include "converter.h"
+#define BUFF_SIZE 100000000
+
+int	main(void)
+{
+	char *str;
+	int **grid;
+	int i;
+	int j;
+
+	str = (char*)malloc(sizeof(char) * BUFF_SIZE);
+	read(0, str, BUFF_SIZE);
+	if (!ft_check_map_length(str) || !ft_check_valid_char(str) || !ft_check_free_space(str)
+		|| !ft_get_rows(str))
+	{
+		printf("map error\n");
+	}
+
+	grid = (int**)malloc(sizeof(int*) * (ft_get_rows(str) + 1));
+	i = 0;
+	while (i < ft_check_map_length(str) + 1)
+	{
+		grid[i] = (int*)malloc(sizeof(int) * (ft_check_map_length(str) + 1));
+		i++;
+	}
+	ft_pad_grid(grid, str);
+	ft_fill_int_grid(grid, str);
+
+	i = 0;
+	j = 0;
+	while (i < ft_get_rows(str) + 1)
+	{
+		while (j < ft_check_map_length(str) + 1)
+		{
+			printf("%d ", grid[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+		j = 0;
+	}
+
+
+	free(str);
+	free(grid);
+	return (0);
+}
